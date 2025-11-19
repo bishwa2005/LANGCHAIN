d6b62883741273f411a71b27f9e9c771d08eb95e
bishwa2005 <br180905@gmail.com>
2025-11-19 23:37:27 +0530

    first commit

--- MODELS/LLM/1_llm_demo.py
+++ MODELS/LLM/1_llm_demo.py
@@ -3,8 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-model = ChatGoogleGenerativeAI(model='gemini-2.5-flash-lite')
+model = ChatGoogleGenerativeAI(model='gemini-3-pro-preview')
 
-result = model.invoke('Write poem on INDIA')
+result = model.invoke('what is gen ai?')
 
 print(result.content)
