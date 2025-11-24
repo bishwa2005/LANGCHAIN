d0854b345734bfd9cd5628154afc0cb37dacaba7
bishwa2005 <br180905@gmail.com>
2025-11-24 22:57:13 +0530

    new updates

--- MODELS/LLM/1_llm_demo.py
+++ MODELS/LLM/1_llm_demo.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-model = ChatGoogleGenerativeAI(model='gemini-3-pro-preview')
+model = ChatGoogleGenerativeAI(model='gemini-2.5-flash-lite')
 
 result = model.invoke('what is gen ai?')
 
